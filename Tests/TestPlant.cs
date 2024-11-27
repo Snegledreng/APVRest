de3995d4ab9df3f35a9f242bec9ab3488874d5cd
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2024-11-27 13:22:44 +0100

    Created Test folder with demo test

---
+++ Tests/TestPlant.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APVRest.Tests
+{
+    [TestClass()]
+    public class TestPlant
+    {
+        [TestMethod()]
+        public void CreatePlantTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
