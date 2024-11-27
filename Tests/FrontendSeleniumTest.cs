9cd89f714018c9bfbaa92f5eb9fafec4d9e35a8c
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2024-11-27 13:41:40 +0100

    Template tests that assert.fail

---
+++ Tests/FrontendSeleniumTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APVRest.Tests
+{
+    [TestClass()]
+    public class FrontendSeleniumTest
+    {
+        /*
+         * Steps:
+         * 01: Create user
+         * 02: Log in
+         * 03: Create plant
+         * 04: View dashboard info on plant
+         * 05: Click detailed info on plant
+         * 06: Ensure all data on plant exists and fields are populated
+         * 07: Edit plant (Name only?)
+         * 08: Make sure name has changed in detailed view
+         * 09: Delete plant
+         * 10: Check dashboard that plant no longer exists
+         * 11: Update user (Name only?)
+         * 12: Ensure user is updated
+         * 13: Delete user
+         */
+
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void LogInTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void CreatePlantTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void GoToDashboardTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void DetailedViewTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void EnsureFieldsInDetailedViewOfAGivenPlantArePopulatedTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void EditPlantTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void EnsureNameHasChangedTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void DeletePlantTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void PlantNoLongerExistsOnDashboardTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void UpdateUserTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void DeleteUserTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Tests/UserTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APVRest.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APVRest.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void DeleteUserTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetHttpTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetUserByIdTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void LogInTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void LogOutTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void UpdateUserTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
