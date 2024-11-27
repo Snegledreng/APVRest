5d23b5a01f49447b23c2c3f6aec1b41830eae39d
Stéphane Karlsen <stk002@edu.zealand.dk>
2024-11-27 10:36:47 +0100

    Create UnitTest

--- Model/Logs.cs
+++ Model/Logs.cs
@@ -2,5 +2,11 @@
 {
     public class Logs
     {
+        public int LogID { get; set; }
+        public int PlantID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Humidity { get; set; }
+        public int RemainingWater { get; set; }
+
     }
 }
--- Model/Plants.cs
+++ Model/Plants.cs
@@ -2,5 +2,19 @@
 {
     public class Plants
     {
+        public int PlantID { get; set; }
+        public string Name { get; set; }
+
+        public int DesiredHumidity { get; set; }
+        public int WaterTankVolume { get; set; }
+        public string IPAddress { get; set; }
+        public int UserId { get; set; }
+        public string PicturUrl { get; set; }
+        public List<Logs> LogListe { get; set; }
+
+        public Plants()
+        {
+            
+        }
     }
 }
